c5f6eb18ba8fba85c3f4323787478bc6c8d1fa10
Maria <mariarosekesa@gmail.com>
2023-03-01 09:24:55 +0200

    multiple plots and pcs

--- app_legacy.py
+++ app_legacy.py
@@ -15,9 +15,13 @@ import matplotlib.pyplot as plt
 from one.api import ONE
 from one.alf.exceptions import ALFObjectNotFound
 from ibllib.atlas import AllenAtlas
+from ibllib.atlas import atlas as at
+from one.api import ONE
+import numpy as np
 
 app = Flask(__name__)
 one = ONE(base_url='https://openalyx.internationalbrainlab.org',password='international', silent=True)
+atlas=at.AllenAtlas()
 
 @app.route('/')
 def home():
@@ -25,22 +29,29 @@ def home():
     return render_template('experiments.html', eids=eids)
 
 def generate_mlapdv_plot(mlapdv):
-    from ibllib.atlas import atlas as at
-    from one.api import ONE
-    import numpy as np
+    
 
-    atlas=at.AllenAtlas()
-    fig, axs = plt.subplots(2, 1, figsize=(10, 10))
-    xyz=np.c_[mlapdv[:,0].astype(np.float64) / 1e6, mlapdv[:,1].astype(np.float64) / 1e6, mlapdv[:,2].astype(np.float64) / 1e6]
-    #fig,axs=atlas.plot_tilted_slice(xyz,0)
-    axs[0].scatter(mlapdv[:,1],mlapdv[:,2],c='b',s=1)
-    #axs[1]=atlas.plot_tilted_slice(xyz,1)
-    axs[1].scatter(mlapdv[:,0],mlapdv[:,2],c='b',s=1)
-    buf = io.BytesIO()
-    plt.savefig(buf, format='png')
-    plt.close(fig)
+    # create the xyz array
+    xyz = np.c_[mlapdv[:, 0].astype(np.float64) / 1e6, mlapdv[:, 1].astype(np.float64) / 1e6, mlapdv[:, 2].astype(np.float64) / 1e6]
 
-    return base64.b64encode(buf.getbuffer()).decode('ascii')
+    # create a figure with subplots
+    fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+
+    # render the first image
+    ax = atlas.plot_tilted_slice(xyz, 0, ax=axs[0], volume='image', cmap='gray')
+    ax.scatter(mlapdv[:, 1], mlapdv[:, 2], c='b', s=1)
+
+    # render the second image
+    ax = atlas.plot_tilted_slice(xyz, 1, ax=axs[1], volume='image', cmap='gray')
+    ax.scatter(mlapdv[:, 0], mlapdv[:, 2], c='b', s=1)
+
+    # save the figure to a buffer
+    buf = BytesIO()
+    fig.savefig(buf, format='png')
+    buf.seek(0)
+
+    # return the image buffer
+    return buf
 
 
 
@@ -58,11 +69,11 @@ def show_mlapdv(eid):
     except ALFObjectNotFound:
         mlapdv_probe01 = None
     
-    # Create the matplotlib figure
-    fig = generate_mlapdv_plot(mlapdv_probe00)
-    
-    # Render the mlapdv_matplotlib.html template with the figure and eid
-    return render_template('mlapdv.html', eid=eid, fig=fig)
+    # render the images for the given eid
+    buf = generate_mlapdv_plot(mlapdv_probe00)
+
+    # render the template with the image buffer
+    return render_template('images.html', eid=eid, image=url_for('static', filename='img.png'), buffer=buf.read())
     
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ app_main.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, url_for
+from one.api import ONE
+from one.alf.exceptions import ALFObjectNotFound, ALFMultipleCollectionsFound
+import io
+from flask import make_response
+import matplotlib.pyplot as plt
+import numpy as np
+import brainbox as bb
+from ibllib.atlas import atlas as at
+import matplotlib.pyplot as plt
+from one.api import ONE
+from one.alf.exceptions import ALFObjectNotFound
+from ibllib.atlas import AllenAtlas
+from ibllib.atlas import atlas as at
+from one.api import ONE
+import numpy as np
+
+app = Flask(__name__)
+one = ONE(base_url='https://openalyx.internationalbrainlab.org',password='international', silent=True)
+
+@app.route('/')
+def home():
+    eids = one.search(data='channels.mlapdv')
+    return render_template('experiments.html', eids=eids)
+
+def generate_mlapdv_plot(mlapdv,ind):
+    atlas=at.AllenAtlas()
+
+    # create the xyz array
+    xyz = np.c_[mlapdv[:, 0].astype(np.float64) / 1e6, mlapdv[:, 1].astype(np.float64) / 1e6, mlapdv[:, 2].astype(np.float64) / 1e6]
+
+    fit=at.Trajectory.fit(xyz)
+    proj=fit.project(xyz)
+
+    # create a figure with subplots
+    fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+    
+
+    # render the first image
+    ax0 = atlas.plot_tilted_slice(xyz, 0, ax=axs[0])
+    ax0[0].scatter(proj[:, 1]*1e6, proj[:, 2]*1e6, c='pink', s=5)
+
+    # render the second image
+    ax1 = atlas.plot_tilted_slice(xyz, 1, ax=axs[1])
+    ax1[0].scatter(proj[:, 0]*1e6, proj[:, 2]*1e6, c='pink', s=5)
+
+    # Save the plot to a file
+    fig.savefig(f'static/mlapdv_plot{ind}.png', bbox_inches='tight')
+
+    # Return the path to the saved file
+    return f'static/mlapdv_plot{ind}.png'
+
+
+
+@app.route('/<eid>')
+def show_mlapdv(eid):
+    # Load mlapdv.npy for probe00 if it exists
+    try:
+        mlapdv_probe00 = one.load_dataset(eid, 'channels.mlapdv.npy', collection=f'alf/probe00/pykilosort')
+    except ALFObjectNotFound:
+        mlapdv_probe00 = None
+
+    # Load mlapdv.npy for probe01 if it exists
+    try:
+        mlapdv_probe01 = one.load_dataset(eid, 'channels.mlapdv.npy', collection=f'alf/probe01/pykilosort')
+    except ALFObjectNotFound:
+        mlapdv_probe01 = None
+    
+    # render the images for the given eid
+    try:
+        path00 = generate_mlapdv_plot(mlapdv_probe00,'00')
+    except:
+        path00=None
+    try:
+        path01 = generate_mlapdv_plot(mlapdv_probe01,'01')
+    except:
+        path01=None
+
+    # render the template with the image buffer
+    return render_template('images.html', plot_path00=path00, plot_path01=path01)
+    
+if __name__ == '__main__':
+    app.run(debug=True)
+
